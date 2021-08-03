2facec4e63d60ee15b6d3e004ae20aaadcec3fdc
krankenberg <76975267+krankenberg@users.noreply.github.com>
2021-08-04 01:20:46 +0200

    add colliding camera

--- Assets/Camera/FollowObjectWithVelocity.cs
+++ Assets/Camera/FollowObjectWithVelocity.cs
@@ -1,3 +1,4 @@
+using System;
 using Car;
 using UnityEngine;
 
@@ -10,12 +11,14 @@ namespace Camera
 
         public float MaxLookAhead = 5F;
         public float MaxVelocity = 10F;
+        public float CollisionDistance = 1F;
 
         private float _currentLookAhead;
         private UnityEngine.Camera _camera;
         private CarMovement _carMovement;
         private Transform _carTransform;
         private Transform _cameraTransform;
+        private RaycastHit[] _hits;
 
         private void Start()
         {
@@ -23,15 +26,37 @@ namespace Camera
             _cameraTransform = _camera.transform;
             _carMovement = GameObject.FindWithTag("Player").GetComponent<CarMovement>();
             _carTransform = _carMovement.transform;
+            _hits = new RaycastHit[10];
         }
 
-        private void LateUpdate()
+        private void FixedUpdate()
         {
             var currentVelocity = _carMovement.GetCurrentVelocity();
+            var directionModifier = currentVelocity > 0 ? 1 : -1;
             var carsPosition = _carTransform.position;
 
-            _currentLookAhead = Mathf.Lerp(_currentLookAhead, currentVelocity / MaxVelocity * MaxLookAhead, Time.deltaTime);
-            _cameraTransform.localPosition = new Vector3(carsPosition.x, CameraDistanceToFloor, carsPosition.z) + _carTransform.forward * _currentLookAhead;
+            _currentLookAhead = Mathf.Lerp(_currentLookAhead, currentVelocity / MaxVelocity * MaxLookAhead, Time.fixedDeltaTime);
+
+            var collisionBox = new Vector3(CollisionDistance, CollisionDistance * 4, CollisionDistance);
+            var collisionCount = Physics.BoxCastNonAlloc(_cameraTransform.position, collisionBox, _carTransform.forward * directionModifier, _hits, Quaternion.identity, Math.Abs(_currentLookAhead));
+            if (collisionCount > 0)
+            {
+                var lowestDistance = float.MaxValue;
+                for (int i = 0; i < collisionCount; i++)
+                {
+                    var distance = _hits[i].distance;
+                    if (distance < lowestDistance)
+                    {
+                        lowestDistance = distance;
+                    }
+                }
+
+                _currentLookAhead = lowestDistance * directionModifier;
+            }
+
+            var carPositionOnCameraLayer = new Vector3(carsPosition.x, CameraDistanceToFloor, carsPosition.z);
+            var targetPosition = carPositionOnCameraLayer + _carTransform.forward * _currentLookAhead;
+            _cameraTransform.localPosition = targetPosition;
         }
     }
 }
