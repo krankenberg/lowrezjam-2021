0c6949733dcd6ec6e9093bab1c77c862ec40746e
krankenberg <76975267+krankenberg@users.noreply.github.com>
2021-08-07 02:36:19 +0200

    refactor FollowObjectWithVelocity

--- Assets/Camera/FollowObjectWithVelocity.cs
+++ Assets/Camera/FollowObjectWithVelocity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Car;
 using UnityEngine;
 
@@ -7,15 +8,13 @@ namespace Camera
     [RequireComponent(typeof(UnityEngine.Camera))]
     public class FollowObjectWithVelocity : MonoBehaviour
     {
-        private const float CameraDistanceToFloor = 9.237604F; // x = (32 / tan(fovDeg / 2)) / PixelsPerUnit
-
         public float MaxLookAhead = 5F;
         public float MaxVelocity = 10F;
         public float CollisionDistance = 1F;
         public float DampTime = 0.5F;
         public float DampMaxSpeed = 0.1F;
 
-        private float _currentLookAhead;
+        private float _cameraDistanceToFloor;
         private UnityEngine.Camera _camera;
         private CarMovement _carMovement;
         private Transform _carTransform;
@@ -30,37 +29,47 @@ namespace Camera
             _carMovement = GameObject.FindWithTag("Player").GetComponent<CarMovement>();
             _carTransform = _carMovement.transform;
             _hits = new RaycastHit[10];
+            _cameraDistanceToFloor = (32 / Mathf.Tan(_camera.fieldOfView * Mathf.Deg2Rad / 2)) / GlobalGameState.PixelsPerUnit;
         }
 
         private void FixedUpdate()
         {
             var currentVelocity = _carMovement.GetCurrentVelocity();
-            var directionModifier = currentVelocity > 0 ? 1 : -1;
-            var carsPosition = _carTransform.position;
 
-            _currentLookAhead = currentVelocity / MaxVelocity * MaxLookAhead;
+            var desiredLookAhead = currentVelocity / MaxVelocity * MaxLookAhead;
+            var lookAhead = ClampDesiredLookAheadToCollidingBuildings(currentVelocity, desiredLookAhead);
+
+            var targetPosition = CalculateTargetPosition(lookAhead);
+            SmoothStepCameraToTargetPosition(targetPosition);
+        }
+
+        private float ClampDesiredLookAheadToCollidingBuildings(float currentVelocity, float desiredLookAhead)
+        {
+            var drivingBackwards = currentVelocity > 0;
+            var directionModifier = drivingBackwards ? 1 : -1;
 
             var collisionBox = new Vector3(CollisionDistance, CollisionDistance * 4, CollisionDistance);
             var collisionCount = Physics.BoxCastNonAlloc(_cameraTransform.position, collisionBox, _carTransform.forward * directionModifier, _hits,
-                Quaternion.identity, Math.Abs(_currentLookAhead));
+                Quaternion.identity, Math.Abs(desiredLookAhead));
             if (collisionCount > 0)
             {
-                var lowestDistance = float.MaxValue;
-                for (int i = 0; i < collisionCount; i++)
-                {
-                    var distance = _hits[i].distance;
-                    if (distance < lowestDistance)
-                    {
-                        lowestDistance = distance;
-                    }
-                }
-
-                _currentLookAhead = lowestDistance * directionModifier;
+                var lowestDistance = _hits.Take(collisionCount).Min(hit => hit.distance);
+                return lowestDistance * directionModifier;
             }
 
-            var carPositionOnCameraLayer = new Vector3(carsPosition.x, CameraDistanceToFloor, carsPosition.z);
-            var targetPosition = carPositionOnCameraLayer + _carTransform.forward * _currentLookAhead;
+            return desiredLookAhead;
+        }
 
+        private Vector3 CalculateTargetPosition(float lookAhead)
+        {
+            var carsPosition = _carTransform.position;
+            var carPositionOnCameraLayer = new Vector3(carsPosition.x, _cameraDistanceToFloor, carsPosition.z);
+            var targetPosition = carPositionOnCameraLayer + _carTransform.forward * lookAhead;
+            return targetPosition;
+        }
+
+        private void SmoothStepCameraToTargetPosition(Vector3 targetPosition)
+        {
             _cameraTransform.localPosition = Vector3.SmoothDamp(_cameraTransform.localPosition, targetPosition, ref _currentDampVelocity, DampTime,
                 DampMaxSpeed, Time.fixedDeltaTime);
         }
