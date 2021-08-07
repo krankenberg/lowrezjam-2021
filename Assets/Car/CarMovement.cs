89d9e3105f623c2e5dd687761dd9ff07ffca1634
krankenberg <76975267+krankenberg@users.noreply.github.com>
2021-08-07 02:11:48 +0200

    refactor CarMovement

--- Assets/Car/CarMovement.cs
+++ Assets/Car/CarMovement.cs
@@ -28,26 +28,63 @@ namespace Car
 
         private void Update()
         {
+            UpdateVelocity();
+            UpdateRotation();
+        }
+
+        private void UpdateVelocity()
+        {
             _currentVelocity = GetCurrentVelocity();
 
             var vertical = Input.GetAxisRaw("Vertical");
-            var horizontal = Input.GetAxisRaw("Horizontal");
 
             if (Math.Abs(vertical) > GlobalGameState.Tolerance)
             {
-                _currentVelocity += vertical * AccelerationSpeed * Time.deltaTime;
+                AccelerateOrDecelerate(vertical);
             }
             else
             {
-                _currentVelocity = Mathf.MoveTowards(_currentVelocity, 0, NaturalDecelerationSpeed * Time.deltaTime);
+                RollToStop();
             }
 
-            _currentVelocity = Mathf.Clamp(_currentVelocity, -MaxBackwardsVelocity, MaxVelocity);
             _rigidbody.velocity = _transform.rotation * new Vector3(0, 0, _currentVelocity);
+        }
 
+        private void AccelerateOrDecelerate(float vertical)
+        {
+            _currentVelocity += vertical * AccelerationSpeed * Time.deltaTime;
+            _currentVelocity = Mathf.Clamp(_currentVelocity, -MaxBackwardsVelocity, MaxVelocity);
+        }
+
+        private void RollToStop()
+        {
+            _currentVelocity = Mathf.MoveTowards(_currentVelocity, 0, NaturalDecelerationSpeed * Time.deltaTime);
+        }
+
+        private void UpdateRotation()
+        {
+            var horizontal = Input.GetAxisRaw("Horizontal");
+
+            var rotationModifier = EvaluateRotationModifier();
+
+            var rotationChange = rotationModifier * horizontal * RotationSpeed * Time.deltaTime;
+            if (Math.Abs(rotationChange) > RotationDeadZone)
+            {
+                _transform.Rotate(Vector3.up, rotationChange, Space.World);
+            }
+            else
+            {
+                SnapRotationToLockingAngle();
+            }
+        }
+
+        private float EvaluateRotationModifier()
+        {
             var drivingBackwards = _currentVelocity < 0;
             var relevantMaxVelocity = drivingBackwards ? MaxBackwardsVelocity : MaxVelocity;
 
+            // Rotation speed is getting faster between 0 velocity and RotationDiminishmentPoint up to RotationDiminishmentPointModifier * RotationSpeed
+            // after RotationDiminishmentPoint the Rotation speed modifier shrinks back to 1, thus Rotation Speed at MaxVelocity will be 1 * RotationSpeed
             float rotationModifier;
             var absVelocity = Math.Abs(_currentVelocity);
             if (absVelocity < RotationDiminishmentPoint)
@@ -62,20 +99,16 @@ namespace Car
             }
 
             rotationModifier *= drivingBackwards ? -1 : 1;
+            return rotationModifier;
+        }
 
-            var rotationValue = rotationModifier * horizontal * RotationSpeed * Time.deltaTime;
-            if (Math.Abs(rotationValue) > RotationDeadZone)
-            {
-                _transform.Rotate(Vector3.up, rotationValue, Space.World);
-            }
-            else
-            {
-                var currentYAngle = _transform.rotation.eulerAngles.y;
-                var newYAngle = Mathf.RoundToInt(currentYAngle / LockingAngle) * LockingAngle;
-                var yAngleDiff = newYAngle - currentYAngle;
+        private void SnapRotationToLockingAngle()
+        {
+            var currentYAngle = _transform.rotation.eulerAngles.y;
+            var newYAngle = Mathf.RoundToInt(currentYAngle / LockingAngle) * LockingAngle;
+            var yAngleDiff = newYAngle - currentYAngle;
 
-                _transform.Rotate(Vector3.up, yAngleDiff, Space.World);
-            }
+            _transform.Rotate(Vector3.up, yAngleDiff, Space.World);
         }
 
         public float GetCurrentVelocity()
